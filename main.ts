6eb36d152dd2b1d893f2421836698bc19e69595f
daaviidpr <125254584+daaviidpr@users.noreply.github.com>
2023-02-13 09:29:02 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.AB, function () {
+    let Corazón: Image = null
+    Corazón.showImage(1)
+})
